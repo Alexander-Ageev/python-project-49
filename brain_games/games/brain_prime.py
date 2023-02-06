76d5fc5e01e7ecfbaf848234de64d3f22b2762ea
Alexander <alexandrageev87@gmail.com>
2023-02-06 22:47:57 +0300

    fix description and script call

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+"""
+Реализация игры "Простое ли число?".
+Игроку показывается случайное число. Он должен ответить yes,
+если это число является простым, или no, если число простым не является.
+"""
+
+
 import brain_games.tools.game as game
 from brain_games.tools.games_logic import prime
 
